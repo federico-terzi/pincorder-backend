43020f86e0b2a80ede8563929ef88a9027162d57
Federico Terzi <freddy6896@gmail.com>
2017-04-08 13:33:05 +0200

    Fixed Urls

--- audio_recorder/audio_recorder/urls.py
+++ audio_recorder/audio_recorder/urls.py
@@ -15,12 +15,8 @@ Including another URLconf
 """
 from django.conf.urls import url, include
 from django.contrib import admin
-from rest_framework_swagger.views import get_swagger_view
-
-schema_view = get_swagger_view(title='PinCorder API')
 
 urlpatterns = [
-    #url(r'^api/$', schema_view),
     url(r'^api/', include('recorder_engine.urls')),
     url(r'^admin/', admin.site.urls),
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
