a919b080729b07332346e58304fcf1366fd45877
Federico Terzi <freddy6896@gmail.com>
2017-04-18 21:08:54 +0200

    Added login page draft

--- pincorder/pincorder_desktop/urls.py
+++ pincorder/pincorder_desktop/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 # Define the url patterns for the Desktop Version
 urlpatterns = [
-    url(r'^$', views.home, name='home')
+    url(r'^$', views.home, name='home'),
+    url(r'^login/$', views.login, name='login')
 ]
--- pincorder/pincorder_desktop/views.py
+++ pincorder/pincorder_desktop/views.py
@@ -3,3 +3,7 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'pincorder_desktop/templates/home.html')
+
+
+def login(request):
+    return render(request, 'pincorder_desktop/templates/security/login.html')
