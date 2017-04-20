279c786de2ec8163e3164a0c24adb037823b6788
Federico Terzi <freddy6896@gmail.com>
2017-04-20 20:52:06 +0200

    Added parent_course to the CourseSerializer

--- pincorder/recorder_engine/serializers.py
+++ pincorder/recorder_engine/serializers.py
@@ -29,7 +29,7 @@ class CourseSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Course
-        fields = ('id', 'name', 'teacher')
+        fields = ('id', 'name', 'teacher', 'parent_course')
 
 
 class PinSerializer(serializers.ModelSerializer):
