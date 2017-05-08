60ca65691ef89722728465d60378ba36c51b18ee
Federico Terzi <freddy6896@gmail.com>
2017-05-08 23:19:24 +0200

    Improved Recording Admin interface

--- pincorder/recorder_engine/admin.py
+++ pincorder/recorder_engine/admin.py
@@ -2,12 +2,43 @@ from django.contrib import admin
 from .models import *
 
 
+class RecordingFileInline(admin.TabularInline):
+    """
+    Used to display Recording File inline in the recording admin page
+    """
+    model = RecordingFile
+
+
 class RecordingAdmin(admin.ModelAdmin):
     """
     Used to display Recordings information in the Django Admin
     """
     list_display = ('name', 'date', 'course', 'is_online', 'is_converted', 'recordingfile')
     search_fields = ('name', 'course')
+    fieldsets = (
+        (None, {
+            'fields': ('name', 'date', 'course', 'privacy', 'user')
+        }),
+        ('Status Fields', {
+            'fields': ('status', 'is_online', 'is_converted')
+        }),
+    )
+    inlines = (RecordingFileInline,)
+
+    def get_form(self, request, obj=None, **kwargs):
+        """
+        Override default method to automatically include current user
+        """
+        # Get the form
+        form = super(RecordingAdmin, self).get_form(request, obj, **kwargs)
+
+        # If obj is None, the recording is being created, so change the default user
+        if obj is None:
+            # Set the user that made the request as the initial user
+            form.base_fields['user'].initial = request.user
+
+        # Return the form
+        return form
 
 
 class CourseAdmin(admin.ModelAdmin):
